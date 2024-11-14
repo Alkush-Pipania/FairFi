8a1376d3306071240349e2fa7ad0d2896957dad3
alkush pipania <alkushpipania2006@gmail.com>
2024-11-15 00:56:20 +0530

    create Product

--- src/app/dashboard/_components/forms/ProductDetailsForm.tsx
+++ src/app/dashboard/_components/forms/ProductDetailsForm.tsx
@@ -1,33 +1,99 @@
-import { Form, useForm } from "react-hook-form";
+"use client"
+import {useForm} from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from '@hookform/resolvers/zod';
-import { FormField, FormItem } from "@/components/ui/form";
+import { FormControl, Form ,  FormField, FormItem, FormLabel, FormMessage, FormDescription } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Description } from "@radix-ui/react-toast";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { ProductDetailsSchema } from "@/schemas/product";
+import { createProduct } from "@/server/actions/products";
+import { useToast } from "@/hooks/use-toast";
 
 
-const ProductDetailsSchema = z.object({
-  name : z.string()
-})
 
-export function ProductDetailsForm(){
+
+export function ProductDetailsForm() {
+  const { toast } = useToast()
   const form = useForm<z.infer<typeof ProductDetailsSchema>>({
-    resolver : zodResolver(ProductDetailsSchema),
+    resolver: zodResolver(ProductDetailsSchema),
+    defaultValues : {
+      name: "",
+      url : "",
+      description : "",
+    },
   })
 
-  function onSubmit(values : z.infer<typeof ProductDetailsSchema>){
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof ProductDetailsSchema>) {
+    const data = await createProduct(values)
+
+    if (data?.message) {
+      toast({
+        title: data.error ? "Error" : "Success",
+        description: data.message,
+        variant: data.error ? "destructive" : "default",
+      })
+    }
   }
-  return <Form {...form}>
-    <form className="flex gap-6 flex-col"
-    onSubmit={form.handleSubmit(onSubmit)}>
-      <FormField 
-       control={form.control}
-       name="name"
-       render={({field})=>(
-        <FormItem className="flex-grow">
-          
-        </FormItem>
-       )}
-      />
-    </form>
-  </Form>;
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}
+      className="flex gap-6 flex-col">
+        <div className="grid gap-6 grid-cols-1 lg:grid-cols-2">
+        <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Product Name</FormLabel>
+            <FormControl>
+              <Input {...field} />
+            </FormControl>
+            <FormMessage/>
+          </FormItem>
+        )}
+        />
+        <FormField
+        control={form.control}
+        name="url"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Enter your website URL</FormLabel>
+            <FormControl>
+              <Input {...field} />
+            </FormControl>
+            <FormDescription>
+              Include the protocol (http/https) and the full path to the sales page
+            </FormDescription>
+            <FormMessage/>
+          </FormItem>
+        )}
+        />
+        </div>
+        <FormField
+        control={form.control}
+        name="description"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Product Description</FormLabel>
+            <FormControl>
+              <Textarea className="min-h-20 resize-none" {...field} />
+            </FormControl>
+            <FormDescription>
+              An optional description to help distinguish your product from other 
+              products
+            </FormDescription>
+            <FormMessage/>
+          </FormItem>
+        )}
+        />
+        <div className="self-end" >
+           <Button disabled={form.formState.isSubmitting} type="submit">
+            Save
+           </Button>
+        </div>
+      </form>
+    </Form>
+  )
 }
---
+++ src/schemas/product.ts
@@ -0,0 +1,9 @@
+import { removeTrailingSlash } from "@/lib/utils";
+import { z } from "zod";
+
+
+export const ProductDetailsSchema = z.object({
+  name: z.string().min(1,"Required"),
+  url: z.string().min(1,"Required").transform(removeTrailingSlash),
+  description : z.string().optional(),
+})
