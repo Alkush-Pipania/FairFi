4c6aa5a44f2f407db24b9583f0292b789de09ee2
alkush pipania <alkushpipania2006@gmail.com>
2024-11-17 02:11:13 +0530

    Edit Product

--- src/app/dashboard/_components/forms/ProductDetailsForm.tsx
+++ src/app/dashboard/_components/forms/ProductDetailsForm.tsx
@@ -10,23 +10,37 @@ import { Button } from "@/components/ui/button";
 import { ProductDetailsSchema } from "@/schemas/product";
 import { createProduct } from "@/server/actions/products";
 import { useToast } from "@/hooks/use-toast";
+import { updateProduct } from "@/server/actions/products";
 
 
 
 
-export function ProductDetailsForm() {
+export function ProductDetailsForm({
+    product,
+  }: {
+    product?: {
+      id: string
+      name: string
+      description: string | null
+      url: string
+    }
+  }) {
   const { toast } = useToast()
   const form = useForm<z.infer<typeof ProductDetailsSchema>>({
     resolver: zodResolver(ProductDetailsSchema),
-    defaultValues : {
+    defaultValues : product ? {...product , description: product?.description ?? ""} : {
       name: "",
       url : "",
       description : "",
     },
   })
 
+
+
   async function onSubmit(values: z.infer<typeof ProductDetailsSchema>) {
-    const data = await createProduct(values)
+    const action = 
+    product == null ? createProduct : updateProduct.bind(null,product.id)
+    const data = await action(values)
 
     if (data?.message) {
       toast({
